cff980beba13bf215840475774181a8b3db1568e
trite <trite@users.noreply.github.com>
2023-08-05 09:19:37 -0700

    POC for running minimal test shader and outputting the results

--- MainScene.gd
+++ MainScene.gd
@@ -1,9 +1,17 @@
 extends Control
 
-var shaderPath = "res://csa_compute_shader.glsl"
+var shaderPath = "res://get_glsl_globals.glsl"
 
 var rd := RenderingServer.create_local_rendering_device()
 var shader := rd.shader_create_from_spirv(load(shaderPath).get_spirv())
+
+var shader_local_x = 5
+var shader_local_y = 1
+var shader_local_z = 1
+
+var shader_groups_x = 1
+var shader_groups_y = 1
+var shader_groups_z = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,3 +23,49 @@ func _process(_delta):
 	pass
 
 func _on_get_info_pressed():
+	# How many iterations are going to be run
+	var iterations = \
+		shader_local_x \
+		* shader_local_y \
+		* shader_local_z \
+		* shader_groups_x \
+		* shader_groups_y \
+		* shader_groups_z
+
+	# Create an empty float32 array with length equal to the iteration count
+	var data = []
+	data.resize(iterations)
+	data.fill(0)
+
+	# Create the buffer and uniform so we can send and receive data
+	var test_buffer_data = PackedFloat32Array(data)
+	var test_buffer_data_bytes = test_buffer_data.to_byte_array()
+	var test_buffer = rd.storage_buffer_create(test_buffer_data_bytes.size(), test_buffer_data_bytes)
+	var test_uniform = RDUniform.new()
+	test_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	test_uniform.binding = 0 # Assign a new binding
+	test_uniform.add_id(test_buffer)
+
+	# Add uniforms to a uniform set that will be passed to the shader
+	var uniform_set := rd.uniform_set_create([
+		test_uniform
+	], shader, 0)
+
+	# Create a compute pipeline
+	var pipeline := rd.compute_pipeline_create(shader)
+	var compute_list := rd.compute_list_begin()
+	rd.compute_list_bind_compute_pipeline(compute_list, pipeline)
+	rd.compute_list_bind_uniform_set(compute_list, uniform_set, 0)
+	rd.compute_list_dispatch(compute_list, shader_groups_x, shader_groups_y, shader_groups_z)
+	rd.compute_list_end()
+
+	# Submit to GPU and wait for sync
+	# TODO: Docs say that we shouldn't normally sync here,
+	#         so figure that out at some point
+	rd.submit()
+	rd.sync()
+
+	# Read back the data from the buffer
+	var result = rd.buffer_get_data(test_buffer).to_float32_array()
+
+	$VBoxContainer/TextEdit.text = str(result)
--- get_glsl_globals.glsl
+++ get_glsl_globals.glsl
@@ -1,7 +1,7 @@
 #[compute]
 #version 450
 
-layout(local_size_x = 5, local_size_y = 5, local_size_z = 1) in;
+layout(local_size_x = 5, local_size_y = 1, local_size_z = 1) in;
 
 layout(set = 0, binding = 0, std430) restrict buffer TestBuffer {
   float data[];
