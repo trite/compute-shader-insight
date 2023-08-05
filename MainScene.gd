f864cbab644c641560662e21cb7129fa24c2cda7
trite <trite@users.noreply.github.com>
2023-08-05 14:07:27 -0700

    POC for connecting to multiple shader configurations and setting workgroup sizes

--- MainScene.gd
+++ MainScene.gd
@@ -1,33 +1,32 @@
 extends Control
 
-# var simple_shader_path = "res://get_glsl_globals.glsl"
-# var shader_template_path = "res://get_glsl_globals.glsl-template"
-
 var shaders_folder = "res://shaders/"
-# var shader_name = "NotYetInitialized"
 
 var rd := RenderingServer.create_local_rendering_device()
+
+var shader_local_x = 42
+var shader_local_y = 1
+var shader_local_z = 1
 
 var shader_groups_x = 1
 var shader_groups_y = 1
 var shader_groups_z = 1
 
+var shaders = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var shader_files = []
 	var dir = DirAccess.open(shaders_folder)
 	if dir:
 		dir.list_dir_begin()
 		var file_name = dir.get_next()
 		while file_name != "":
 			if file_name.ends_with(".glsl"):
-				shader_files.append(file_name)
+				shaders.append(file_name)
+				$VBox/HBox/ShaderList.add_item(file_name)
 			file_name = dir.get_next()
 	else:
 		push_error("Problem loading list of shaders from shaders folder")
-		# print("Problem loading list of shaders from shaders folder")
-
-
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -35,15 +34,39 @@ func _process(_delta):
 	pass
 
 func _on_get_info_pressed():
+	var shader_name = $VBox/HBox/ShaderList.text
+	var shader_path = shaders_folder + shader_name
+
+	var regex_str = "get_glsl_globals_(?<x_count>\\d+)-(?<y_count>\\d+)-(?<z_count>\\d+)\\.glsl"
+	var regex = RegEx.new()
+	regex.compile(regex_str)
+
+	var regex_result = regex.search(shader_name)
+
+	if regex_result:
+		shader_local_x = int(regex_result.get_string("x_count"))
+		shader_local_y = int(regex_result.get_string("y_count"))
+		shader_local_z = int(regex_result.get_string("z_count"))
+	else:
+		push_error("Problem parsing shader name")
+	
+	shader_groups_x = int($VBox/HBox/xWorkgroupsValue.text)
+	shader_groups_y = int($VBox/HBox/yWorkgroupsValue.text)
+	shader_groups_z = int($VBox/HBox/zWorkgroupsValue.text)
+
+	# for n in regex_result.names:
+	# 	print(n + ": " + regex_result.get_string(n))
+
+	# regex: get_glsl_globals_(?<x_count>\d+)-(?<y_count>\d+)-(?<z_count>\d+)\.glsl
+
 	# var attempt_load = load(temp_shader_name)
-	var attempt_load = load(simple_shader_path)
-	print(attempt_load)
-	var attempt_get_spirv = attempt_load.get_spirv()
+	# var attempt_load = load(simple_shader_path)
+	# print(attempt_load)
+	# var attempt_get_spirv = attempt_load.get_spirv()
 
+	var shader := rd.shader_create_from_spirv(load(shader_path).get_spirv())
 
-	var shader := rd.shader_create_from_spirv(attempt_get_spirv)
-
-	# How many iterations are going to be run
+	# # How many iterations are going to be run
 	var iterations = \
 		shader_local_x \
 		* shader_local_y \
@@ -88,27 +111,5 @@ func _on_get_info_pressed():
 	# Read back the data from the buffer
 	var result = rd.buffer_get_data(test_buffer).to_float32_array()
 
-	$VBoxContainer/TextEdit.text = str(result)
+	$VBox/HBoxBottom/TextEdit.text = str(result)
 
-
-func _on_generate_shader_pressed():
-	var shader_template_content = FileAccess.open(shader_template_path, FileAccess.READ).get_as_text()
-
-	var shader_content = shader_template_content \
-		.replace("{local_size_x}", str(shader_local_x)) \
-		.replace("{local_size_y}", str(shader_local_y)) \
-		.replace("{local_size_z}", str(shader_local_z))
-
-	var now = Time.get_datetime_dict_from_system()
-
-	var now_string = \
-		str(now["year"]) + "-" \
-		+ str(now["month"]) + "-" \
-		+ str(now["day"]) + "_" \
-		+ str(now["hour"]) + "-" \
-		+ str(now["minute"]) + "-" \
-		+ str(now["second"])
-
-	temp_shader_name = temp_shaders_folder + "get_glsl_globals_" + now_string + ".glsl"
-
-	FileAccess.open(temp_shader_name, FileAccess.WRITE).store_string(shader_content)
